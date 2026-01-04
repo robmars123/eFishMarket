35563145446394e1365c08f0acdcceb4b4bdaecd
robmars123 <robmars2008@gmail.com>
2026-01-04 10:38:27 -0600

    update url and vite config

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../api/EFM.Api/wwwroot',
-    chunkSizeWarningLimit: 1024,
-    emptyOutDir: true
-  }
-});
+export default defineConfig({ 
+  build: { outDir: 'build' } 
+})
